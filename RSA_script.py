245e0397dd0b88306c09ab0731913d0353ebcfc1
Priya K. Naphade <pnaphade@della5.princeton.edu>
2021-07-20 16:01:44 -0400

    condensed using loops, general RSA function

--- RSA_script.py
+++ RSA_script.py
@@ -1,124 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+from RSA_functions import RSA, corr_prep
 
-# Load in audio data, transpose in preparation for correlation
-es_spect = np.load('/tigress/pnaphade/es_spect.npy')
+# Load in audio data, transpose in preparation for correlatio
+# Choose the chromagram this time
+es_spect = np.load("/tigress/pnaphade/es_chroma_db.npy")
 es_spect = es_spect.T
 
 # Load in neural data
-masked_data_dir = '/tigress/jamalw/Eternal_Sunshine/scripts/rois/masked_data/'
-music_A1_run1  = np.load(masked_data_dir + 'music/a1plus_run1_n12.npy')
-music_A1_run2 = np.load(masked_data_dir + 'music/a1plus_run2_n12.npy')
-music_rA1_run1  = np.load(masked_data_dir + 'music/rA1_run1_n12.npy')
-music_rA1_run2 = np.load(masked_data_dir + 'music/rA1_run2_n12.npy')
-no_music_A1_run1  = np.load(masked_data_dir + 'no_music/a1plus_run1_n11.npy')
-no_music_A1_run2 = np.load(masked_data_dir + 'no_music/a1plus_run2_n11.npy')
-no_music_rA1_run1  = np.load(masked_data_dir + 'no_music/rA1_run1_n11.npy')
-no_music_rA1_run2 = np.load(masked_data_dir + 'no_music/rA1_run2_n11.npy')
+masked_data_dir = "/tigress/jamalw/Eternal_Sunshine/scripts/rois/masked_data/"
+dir_endings = ["music/a1plus_run1_n12.npy", "music/a1plus_run2_n12.npy", "music/rA1_run1_n12.npy", "music/rA1_run2_n12.npy","no_music/a1plus_run1_n11.npy","no_music/a1plus_run2_n11.npy","no_music/rA1_run1_n11.npy","no_music/rA1_run2_n11.npy"]
 
-# Average, concatenate, and transpose neural data in preparation for correlation
-def corr_prep(run1_data, run2_data) :
+neural_runs = []
+
+for ending in dir_endings :
+	neural_runs.append(np.load(masked_data_dir + ending))
 	
-	# Make sure we're working with data of the correct type and dimensionality
-	if not(isinstance(run1_data, np.ndarray)) or not(isinstance(run2_data, np.ndarray)) :
-		raise TypeError("Data must be a numpy ndarray")
-		
-	if run1_data.ndim != 3 or run2_data.ndim != 3 :
-		raise ValueError("Data must be 3 dimensional (voxels x time x subjects)")
-
-	# Average each run across subjects
-	run1_data_avg = np.mean(run1_data, axis=2)
-	run2_data_avg = np.mean(run2_data, axis=2)
-
-	# Concatenate the data across time
-	avg_concat_data = np.concatenate((run1_data_avg, run2_data_avg), axis=1)
-
-	# Transpose and return
-	return avg_concat_data.T
-
-music_A1_prepped = corr_prep(music_A1_run1, music_A1_run2)
-music_rA1_prepped = corr_prep(music_rA1_run1, music_rA1_run2)
-no_music_A1_prepped = corr_prep(no_music_A1_run1, no_music_A1_run2)
-no_music_rA1_prepped = corr_prep(no_music_rA1_run1, no_music_rA1_run2)
 
 
+# Prepare the neural data for correlation
+neural_prepped = []
 
-# Correlate the neural data across time
-music_A1_corr = np.corrcoef(music_A1_prepped)
-music_rA1_corr = np.corrcoef(music_rA1_prepped)
-no_music_A1_corr = np.corrcoef(no_music_A1_prepped)
-no_music_rA1_corr = np.corrcoef(no_music_rA1_prepped)
+for run, i in zip(neural_runs, np.arange(4)) :
+	neural_prepped.append(corr_prep(neural_runs[2*i], neural_runs[2*i+1]))
 
-# Figure out which rows in the spectrogram have zero variance (results in nans in correlation)
-zero_var_rows = []
-for i in range(6452):
+# Figure out which columns in the spectrogram have zero variance (results in nans in correlation)
+zero_var_cols = []
+for i in range(neural_prepped[0].shape[0]):
 	if np.var(es_spect[i, :]) == 0 :
-		zero_var_rows.append(i)
-
-#print(zero_var_rows)
-
-# Function for chopping off desired values from a symmetric matrix
-def chop(mat, idx_lo, idx_hi) :
-	return mat[idx_lo : idx_hi, idx_lo : idx_hi]
+		zero_var_cols.append(i)
 
 # Chop off the appropriate values
-es_spect_chop = es_spect[4 : 6431, :] 
+es_spect_chop = es_spect[4:6431, :] 
 
-# Correlate the chopped audio data across time
-audio_corr_chop = np.corrcoef(es_spect_chop)	
-
-# Chop the neural matrices to match shape with the audio RSM
-music_A1_corr_chop = chop(music_A1_corr, 4, 6431)
-music_rA1_corr_chop = chop(music_rA1_corr, 4, 6431)
-no_music_A1_corr_chop = chop(no_music_A1_corr, 4, 6431)
-no_music_rA1_corr_chop = chop(no_music_rA1_corr, 4, 6431)
-
-# Group the neural data into a 3D array
-neural_mat = np.asarray([music_A1_corr_chop, music_rA1_corr_chop, no_music_A1_corr_chop, no_music_rA1_corr_chop])
+for dataset, i in zip(neural_prepped, np.arange(4)) : 
+	neural_prepped[i] = dataset[4:6431, :]		
 
 
-# Variables for sliding window correlations
-n_rois = 4
-n_trs = es_spect_chop.shape[0]
-window_width = 30
-n_windows = n_trs - window_width
-RSM_corrs = np.zeros((n_rois, 2, n_windows))
 
-# Calculate 30 s sliding window correlations between the audio RSM and each neural RSM
-for i in range(n_windows) :
+# Perform the RSA
+corrs = np.zeros(4)
+sliding_corrs = []
+RSMs = np.zeros((5, neural_prepped[0].shape[0], neural_prepped[0].shape[0]))
+for area, i in zip(neural_prepped, np.arange(4)) :
+	
+	results = RSA(area, es_spect_chop, sliding_window=True, window_width=30) 
+	
+	# Record the correlations between the current two RSMs
+	corrs[i] = results[2]
 
-	# Isolate the current window for the neural and audio RSMs
-	audio_win = chop(audio_corr_chop, i, i+window_width)
-	neural_win_mat = np.zeros((n_rois, window_width, window_width))
-	for j in range(n_rois) : 
-		neural_win_mat[j] = chop(neural_mat[j], i, i+window_width)
+	# Record the sliding correlations betweeen the two current RSMs
+	sliding_corrs.append(results[4])
 
-	# Pull out the upper triangles of the RSMs for correlation
-	triu_idx = np.triu_indices(window_width)
-	audio_win_triu = audio_win[triu_idx]
-	neural_win_triu_mat = np.zeros((n_rois, audio_win_triu.shape[0]))
-	for j in range(n_rois) :
-		neural_win_triu_mat[j] = neural_win_mat[j][triu_idx]
-		
-	# Compute the Pearson correlation and p value
-	for j in range(n_rois) :
-		RSM_corrs[j, 0, i], RSM_corrs[j, 1, i] = stats.pearsonr(neural_win_triu_mat[j], audio_win_triu)
+	# Record the current neural RSM
+	RSMs[i] = results[0]
+	
+# Record the audio RSM
+RSMs[4] = results[1]
 
-avg_corrs, avg_pvals = np.mean(RSM_corrs[:, 0, :], axis=1), np.mean(RSM_corrs[:, 1, :], axis=1)
+# Convert sliding correlations into an ndarray
+sliding_corrs = np.asarray(sliding_corrs)
 
-# Print the results
-print(f"Neural-Audio Correlations: {np.around(avg_corrs, decimals=5)}")
 
+
+# Plotting
 roi_labels = ["Music Bilateral A1", "Music Right A1", "No Music Bilateral A1", "No Music Right A1"]
-
 
 # Visualize the neural representational similarity matrices
 fig, axes = plt.subplots(2, 2, figsize=(10, 10))
 axes = axes.flatten()
-for i, ax in zip(np.arange(n_rois), axes) :
+for i, ax in zip(np.arange(4), axes) :
 
-	im = ax.imshow(neural_mat[i], cmap='jet')
+	im = ax.imshow(RSMs[i], cmap='jet')
 	ax.set_title(roi_labels[i])
 	
 	if i == 2 or i == 3 :
@@ -131,9 +85,10 @@ fig.subplots_adjust(right = 0.8)
 cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
 fig.colorbar(im, cax=cbar_ax)
 
+
 # Visualize the audio representational similarity matrix
 fig, ax = plt.subplots()
-im = ax.imshow(audio_corr_chop, cmap='jet')
+im = ax.imshow(RSMs[4], cmap='jet')
 ax.set_xlabel("TR")
 ax.set_ylabel("TR")
 fig.subplots_adjust(right = 0.8)
@@ -145,12 +100,12 @@ cbar = fig.colorbar(im, cax=cbar_ax)
 fig, axes = plt.subplots(2, 2, figsize=(15, 12))
 axes = axes.flatten()
 
-for i, ax in zip(np.arange(n_rois), axes) :
+for i, ax in zip(np.arange(4), axes) :
 
-	ax.plot(RSM_corrs[i, 0, :], linewidth=0.5)
+	ax.plot(sliding_corrs[i], linewidth=0.5)
 	ax.set_title(roi_labels[i])
 	ax.set_ylim(0, 0.8)
-	ax.text(5050, 0.72, r'$r_{mean}$' + f" = {np.around(avg_corrs, decimals=4)[i]}")
+	ax.text(5050, 0.72, r'$r_{mean}$' + f" = {np.around(corrs, decimals=4)[i]}")
 		
 	if i == 2 or i == 3 :
 		ax.set_xlabel("TR")
@@ -159,6 +114,7 @@ for i, ax in zip(np.arange(n_rois), axes) :
 		ax.set_ylabel("Neural-Audio Correlation")
 
 
+'''
 # Correlations for final beach house scene
 RSM_corrs_beachhouse = RSM_corrs[:, 0, 5410:5470]
 avg_corrs_beachhouse = np.mean(RSM_corrs_beachhouse, axis=1)
@@ -182,3 +138,4 @@ for i, ax in zip(np.arange(n_rois), axes) :
 		ax.set_ylabel("Neural-Audio Correlation")
 
 #plt.show()
+'''
