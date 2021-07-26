86108b5052b70ac1862921ac711a1b789c105932
Priya K. Naphade <pnaphade@della5.princeton.edu>
2021-07-26 18:38:12 -0400

    included mel_spect correlations, started extracting music scenes

--- scripts/RSA_script.py
+++ scripts/RSA_script.py
@@ -6,9 +6,10 @@ import re
 from pathlib import Path
 import os
 import glob
+import xlrd
 
 
-# Load in the audio features, transpose in preparation for correlation
+#Load in the audio features, transpose in preparation for correlation
 feat_dir = "/tigress/pnaphade/Eternal_Sunshine/results/RSA"
 feat_paths = glob.glob(os.path.join(feat_dir, "es*"))
 features = [np.load(path).T for path in feat_paths]
@@ -23,13 +24,19 @@ A1_data = ["music/a1plus_run1_n12.npy", "music/a1plus_run2_n12.npy", "music/rA1_
 
 control_data = ["music/brainstem_run1_n12.npy", "music/brainstem_run2_n12.npy", "music/occipital_pole_run1_n12.npy", "music/occipital_pole_run2_n12.npy", "no_music/brainstem_run1_n11.npy", "no_music/brainstem_run2_n11.npy", "no_music/occipital_pole_run1_n11.npy", "no_music/occipital_pole_run2_n11.npy"]
 
+
 # Choose which dataset to analyze
-neural_runs = [np.load(masked_dir + run) for run in control_data]
+neural_runs = [np.load(masked_dir + run) for run in A1_data]
+#neural_runs = []
+
+#for i in np.arange(8) :
+	#rand_data = np.random.default_rng().uniform(-2, 2, (1500, 3240, 12))
+	#neural_runs.append(rand_data)
 
 # Labels for displaying results
-#corr_labels = ["Music A1", "Music rA1", "No Music A1", "No Music rA1"]
-corr_labels = ["Music Brainstem", "Music Occipital Pole", "No Music Brainstem", "No Music Occipital Pole"]
-
+corr_labels = ["Music A1", "Music rA1", "No Music A1", "No Music rA1"]
+#corr_labels = ["Music Brainstem", "Music Occipital Pole", "No Music Brainstem", "No Music Occipital Pole"]
+#corr_labels = ["random 1", "random 2", "random 3", "random 4"]
 
 
 
@@ -71,13 +78,7 @@ sliding_corrs = np.zeros((n_feats, n_neurdata, n_trs-30)) #
 # Loop over each audio feature
 for i, feature in enumerate(features) :
 	
-	# Skip over i = 1 because of the many zero variance rows in mel-scaled spectrogram
-	if i == 1 :
-		continue
-
 	print(f"Performing RSA using audio feature {i+1} of {n_feats}")
-	
-	#slide_corrs_temp = []
 	
 	# Loop over each neural dataset
 	for j, neurdata in enumerate(neural_prepped) :
@@ -92,8 +93,7 @@ for i, feature in enumerate(features) :
 
 		# Record the sliding correlations betweeen the two current RSMs
 		sliding_corrs[i, j] = results[4]
-		#slide_corrs_temp.append(results[4])
-		
+			
 	# Record the audio feature RSM
 	RSMs[i, 4] = results[1]
 
@@ -101,25 +101,97 @@ for i, feature in enumerate(features) :
 	#slide_corrs_temp = np.asarray(slide_corrs_temp)
 
 
+# Credits scene regressor
+# The credits are the last 205 seconds of the movie. Extract the last 205-30 = 175
+# seconds of the movie because of the 30s sliding windows
+credits_sliding_corrs = sliding_corrs[:, :, -175:]
+credits_corrs = np.mean(credits_sliding_corrs, axis=2)
+
+master_corrs = [corrs, credits_corrs]
+
 # Print the results
 print("\nResults")
 print("-------\n")
 
-for i, feature in enumerate(features) :
-	
-	# Skip over the mel-scaled spectrogram	
-	if i == 1 :
-		continue
-	
-	print(f"{feat_labels[i]}")
+for h, corrs in enumerate(master_corrs) :
 
-	for j in np.arange(n_neurdata) :
-		print(f"{corr_labels[j]}-Audio correlation: {np.around(corrs[i, j], decimals=4)}")
+	if h == 0 :
+		print("Whole-Movie Correlations")
+	else :
+		print("Credits Scene Correlations")
 
-	if not(i == len(features)-1) :
-		print("\n", end='')
+	for i, feature in enumerate(features) :
+		
+		print(f"{feat_labels[i]}")
+
+		for j in np.arange(n_neurdata) :
+			print(f"{corr_labels[j]}-Audio correlation: {np.around(corrs[i, j], decimals=4)}")
+
+		if not(i == 3 and h == 1) :
+			print("\n", end='')
+
+
+'''
+# Read the Scene Song Notations workbook
+music_times_path = "/tigress/pnaphade/Eternal_Sunshine/data/scene_song_notations.xlsx"
+music_times_wb = xlrd.open_workbook(music_times_path)
+sheet = music_times_wb.sheet_by_index(0)
+
+# Extract the onset and offset columns
+onset_cells = sheet.col(1)
+offset_cells = sheet.col(2)
+
+# Pop the headers of the columns
+onset_cells.pop(0)
+offset_cells.pop(0)
+
+# Extract the values of the columns
+onset_vals = [cell.value for cell in onset_cells]
+offset_vals = [cell.value for cell in offset_cells]
+
+# Function for converting to seconds
+def to_seconds(time):
+	
+	hours, minutes, seconds = time.split(':')
+	
+	total_seconds = int(hours)*3600 + int(minutes)*60 + int(seconds)
+	
+	return total_seconds
+
+
+# Convert the times into seconds
+onset_sec = np.asarray([to_seconds(val) for val in onset_vals])
+offset_sec = np.asarray([to_seconds(val) for val in offset_vals])
+
+# Adjust 4 seconds forward due to beginning silence
+onset_sec, offset_sec = onset_sec-4, offset_sec-4
 '''
 
+'''
+# plot the credits results
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+axes = axes.flatten()
+
+# Skip the mel-scaled spectrogram
+for i, ax in zip(np.asarray([0, 2, 3]), axes) :
+	
+	# plot the music A1 data
+	ax.plot(credits_sliding_corrs[i, 0], 'r-', linewidth=1, label=corr_labels[0])
+
+	# plot the no music A1 data
+	ax.plot(credits_sliding_corrs[i, 2], 'b-', linewidth=1, label=corr_labels[2])
+	
+	ax.set_title(feat_labels[i])
+		
+	ax.set_xlabel("TR")
+	
+	ax.set_ylim(0.2, 0.7)	
+	ax.legend(loc = 'upper left')
+	
+	if i == 0 :
+		ax.set_ylabel("Neural-Audio Correlation")
+'''
+'''
 # Save the between RSM correlations and RSMs
 save_dir = "/tigress/pnaphade/Eternal_Sunshine/results/RSA/"
 roi = "A1"
