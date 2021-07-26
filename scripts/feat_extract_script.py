08bc53140d2f7a0848c9cb01f4839571616a72d2
Priya K. Naphade <pnaphade@della5.princeton.edu>
2021-07-26 17:06:49 -0400

    fixed plotting for mfcc

--- scripts/feat_extract_script.py
+++ scripts/feat_extract_script.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa as lb
+import librosa.display
 import feat_extract_functions as fex
 import pathlib
+
 
 # Load in the movie audio
 audio_filepath = "/tigress/pnaphade/Eternal_Sunshine/data/es_audio.mp3"
@@ -15,7 +17,9 @@ n_feats = len(features)
 
 # Extract the features 
 es_features = []
+
 for feature in features :
+	print(f"Computing {feature}")
 	es_features.append(fex.extract(feature, audio_filepath))
 
 
@@ -37,11 +41,17 @@ for ax, feature, i in zip(axes, es_features, np.arange(n_feats)) :
 	# Use chroma as the y axis for the constant-q chromagram
 	if i == 2 :
 		image = lb.display.specshow(feature, x_axis='time', y_axis='chroma', sr=48000, hop_length=48000, ax=ax)
+		fig.colorbar(image, ax=ax, format="%+2.f dB")
+	
+	# No decibels for mfccs
+	elif i == 3 :
+		image = lb.display.specshow(feature, x_axis='time', sr=48000, hop_length=48000, ax=ax)
+		fig.colorbar(image, ax=ax)
 	
 	else: 
 		image = lb.display.specshow(feature, x_axis='time', y_axis='hz', sr=48000, hop_length=48000, ax=ax)
+		fig.colorbar(image, ax=ax, format="%+2.f dB")
 	
 	ax.set_title(feature_labels[i])	
-	fig.colorbar(image, ax=ax, format="%+2.f dB")
 
 #plt.show()
