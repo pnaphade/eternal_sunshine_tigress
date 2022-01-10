5a4553844e8dd844ca998e0c8992ca83fa2bd939
Priya K. Naphade <pnaphade@della5.princeton.edu>
2022-01-10 13:19:50 -0500

    first commit

---
+++ scripts/ff_theme_credits.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load in the data
+data_dir = "/tigress/pnaphade/Eternal_Sunshine/results/RSA/"
+A1_corrs = np.load(data_dir + "hrf_A1_full_length_slide_corrs.npy")
+bs_corrs = np.load(data_dir + "hrf_control_full_length_slide_corrs.npy")
+
+# Pull out the chromagram row
+A1_corrs = A1_corrs[2, :, :]
+bs_corrs = bs_corrs[2, :, :]
+
+# Music/no music correlations over focus feature theme (rA1, chroma)
+fig1, ax = plt.subplots()
+ax.plot(A1_corrs[1, 0:13], 'b', label="Music") # music rA1
+ax.plot(A1_corrs[3, 0:13], 'r', label="No Music") # no music rA1
+ax.set_ylabel("Correlation")
+ax.set_xlabel("TR")
+ax.legend()
+fig1.show()
+
+# Music/no music correlations over credits
+fig2, ax = plt.subplots()
+ax.plot(A1_corrs[1, -204:], 'b', label="Music") # music rA1
+ax.plot(A1_corrs[3, -204:], 'r', label="No Music") # no music rA1
+ax.set_ylabel("Correlation")
+ax.set_xlabel("TR")
+ax.legend()
+fig2.show()
