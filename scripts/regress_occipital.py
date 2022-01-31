b7ef125e54c7d227a8d94c01aafb3c7b56ae487f
Priya K. Naphade <pnaphade@della5.princeton.edu>
2022-01-31 01:13:00 -0500

    added loop for no music regression

--- scripts/regress_occipital.py
+++ scripts/regress_occipital.py
@@ -28,8 +28,33 @@ for i in np.arange(rA1_music.shape[2]) :
 	rA1_music_reg = rA1_music_sub - np.dot(reg.coef_, occ_music_sub) - reg.intercept_[:, np.newaxis]
 
 
-	# Save the results
+	# Record the results
 	rA1_music_regressed = np.zeros_like(rA1_music)
 	rA1_music_regressed[:, :, i] = rA1_music_reg
 
+
 # Regress out occipital pole signal for no music subjects
+for i in np.arange(rA1_nomusic.shape[2]) :
+
+	# Pull out the data for each subject
+	occ_nomusic_sub = occ_nomusic[:, :, i]
+	rA1_nomusic_sub = rA1_nomusic[:, :, i]
+
+	# Progress update
+	print("Performing regression on no music subject " + str(i))
+
+	# Create and fit the regression model using occipital pole as the predictor
+	reg = linear_model.LinearRegression()
+	reg.fit(occ_nomusic_sub.T, rA1_nomusic_sub.T) # transpose so we have time x voxels
+
+	# Subtract the part of the rA1 signal that can be explained by occipital pole signal
+	rA1_nomusic_reg = rA1_nomusic_sub - np.dot(reg.coef_, occ_nomusic_sub) - reg.intercept_[:, np.newaxis]
+
+
+	# Record the results
+	rA1_nomusic_regressed = np.zeros_like(rA1_nomusic)
+	rA1_nomusic_regressed[:, :, i] = rA1_nomusic_reg
+
+# Save the rA1 signal with occipital pole regressed out
+np.save(datadir + "music/rA1_regressed.npy", rA1_music_regressed)
+np.save(datadir + "no_music/rA1_regressed.npy", rA1_nomusic_regressed)
